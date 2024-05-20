4a359cdee4597f73e1aa8744679f15d1a933eb8a
Anushtha <kumarsen.bash@gmail.com>
2024-05-20 20:55:20 +0530

    apis

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://192.168.0.105:5000/api/v1/";
+axios.defaults.baseURL = "https://find-my-partner-backend.onrender.com/api/v1/";
 
 export default axios;
